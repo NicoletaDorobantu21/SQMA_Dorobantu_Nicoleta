e9449dc6f069da3c8e7df11c4af50ab72b1ab9a1
NicoletaDorobantu21 <91662385+NicoletaDorobantu21@users.noreply.github.com>
2024-12-15 12:13:18 +0200

    Update unityTest.java

--- unityTest.java
+++ unityTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+public class unityTest {
 
     private final Calculator calculator = new Calculator();
 
